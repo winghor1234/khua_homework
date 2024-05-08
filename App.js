77602e3d459633633bbc2276a52efd911787d818
Khua Yiachingchuethor <154345548+winghor1234@users.noreply.github.com>
2024-05-08 11:13:03 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,156 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {TextInput,TouchableOpacity, StyleSheet,Image, Text, View } from 'react-native';
 
-export default function App() {
+
+
+
+
+
+const App = () => {
+
+  const handlerLongClick = () => {
+    //handler for Long Click
+    alert('Button Long Pressed');
+  };
+
+  const handlerClick = () => {
+    //handler for Long Click
+    alert('Button Pressed');
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+
+         
+        
+      <Text activeOpacity={0.6} style = {styles.text} onPress={handlerClick} onLongPress={handlerLongClick}
+      onStartShouldSetResponder={() => alert('View Click')}>
+        Example of Click on a View!!
+     
+      </Text>
+      
+      <TouchableOpacity onLongPress={handlerLongClick} onPress={handlerClick}
+      activeOpacity={0.6}
+      style={styles.buttonStyle}>
+        <Text style = {styles.buttonTextStyle}>LONG PRESS THE BUTTON</Text>
+      </TouchableOpacity>
+      <TextInput
+          style={styles.textInputStyle}
+          placeholder="Enter Numeric Values Only"
+          placeholderTextColor="#60605e"
+          numeric
+          keyboardType={'numeric'}
+        />
+    
+      <TouchableOpacity
+          style={styles.buttonFacebookStyle}
+          activeOpacity={0.5}>
+          <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/facebook.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle} />
+          <Text style={styles.buttonTextStyle}>
+            Login Using Facebook
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.buttonGPlusStyle} 
+          activeOpacity={0.5} > 
+          <Image
+            source={{
+              uri:
+                'https://raw.githubusercontent.com/AboutReact/sampleresource/master/google-plus.png',
+            }}
+            style={styles.buttonImageIconStyle}
+          />
+          <View style={styles.buttonIconSeparatorStyle}  />
+          <Text style={styles.buttonTextStyle}>
+            Login Using Google Plus
+          </Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
-}
-
+};
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: "gold",
+   padding:10,
+   fontSize:20,
+   fontStyle:'italic',
+   fontWeight:'bold',
+  },
+  buttonStyle:{
+    color:"maroom",
+    textAlign:'center',
+    fontSize:16,
+    fontWeight:'100'
+  },
+  buttonTextStyle: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  buttonGPlusStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#dc4e41',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    width:220,
+    margin: 5,
+  },
+  buttonFacebookStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#485a96',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    width:220,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonImageIconStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+  },
+  buttonTextStyle: {
+    color: '#fff',
+    marginBottom: 4,
+    marginLeft: 10,
+  },
+  buttonIconSeparatorStyle: {
+    backgroundColor: '#fff',
+    width: 1,
+    height: 40,
+  },
+  textInputStyle: {
+    width: 350,
+    backgroundColor: '#dde8c9',
+    padding: 10,
+    margin: 10
+  },
+  spinnerTextStyle: {
+    color: '#FFF',
+  },
+
 });
